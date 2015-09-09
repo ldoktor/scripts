88c6fc4cd5c751d60d5e5c95c727abea853fccd3
Lukáš Doktor <ldoktor@redhat.com>
2015-09-09 19:29:14 +0200

    lenovo_fan.fan_cron: Adjust to W541
    add more levels as "auto" speed is unusable.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- lenovo_fan/fan_cron.sh
+++ lenovo_fan/fan_cron.sh
@@ -3,11 +3,10 @@
 TEMP=`cat /sys/class/thermal/thermal_zone0/temp`
 if [ $TEMP -gt 85000 ]; then
     echo level full-speed > /proc/acpi/ibm/fan
-#elif [ $TEMP -gt 65000 ]; then
-#    echo level auto > /proc/acpi/ibm/fan
-#else
-#    echo level 0 > /proc/acpi/ibm/fan
-#fi
-else
+elif [ $TEMP -gt 70000 ]; then
     echo level auto > /proc/acpi/ibm/fan
+elif [ $TEMP -gt 65000 ]; then
+    echo level 1 > /proc/acpi/ibm/fan
+elif [ $TEMP -lt 55000 ]; then
+    echo level 0 > /proc/acpi/ibm/fan
 fi
