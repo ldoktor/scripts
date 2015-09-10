122342283581ba4e6591b4847ca413417bdb1312
Lukáš Doktor <ldoktor@redhat.com>
2015-09-10 08:43:47 +0200

    cpufreq.cpu_intel_pstate: Add turbo boost handling
    With this patch 0-100 values disable turbo and >100 enables it.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- cpufreq/cpu_intel_pstate.sh
+++ cpufreq/cpu_intel_pstate.sh
@@ -1,5 +1,12 @@
-if [ $# -ne 1 ]; then
+if [ $# -ne 1 ] || [ $1 -ne $1 ] 2>/dev/null; then
     echo "Usage: $0 MAX_SPEED_PERCENTAGE"
+    echo "  MAX_SPEED_PERCENTAGE 0-100 or >100 for turbo boost"
     exit 1
 fi
-echo $1 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+if [ $1 -le 100 ]; then
+    echo $1 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+    echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+else
+    echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+    echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+fi
