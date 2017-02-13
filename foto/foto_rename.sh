60f6bd8e3c1a9440aed602067c651dd206d7ead3
Lukáš Doktor <ldoktor@redhat.com>
2017-02-13 06:05:55 +0100

    foto/foto_rename: Protect variables
    Quote variable to avoid errors.
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- foto/foto_rename.sh
+++ foto/foto_rename.sh
@@ -39,7 +39,7 @@ done
 #OFFSET='0:0:0 00:50:0'
 #exiftool -AllDates+="$shift" -IFD1:ModifyDate+="$shift" -FileModifyDate+="$shift" -overwrite_original_in_place -r .
 #exiftool '-FileName<CreateDate' -d IMG_%Y%m%d_%H%M%S_tata.%%e -overwrite_original_in_place -r .
-[ $OFFSET ] && exiftool -AllDates+="$OFFSET" -IFD1:ModifyDate+="$OFFSET" -FileModifyDate+="$OFFSET" -overwrite_original_in_place -r $*
+[ "$OFFSET" ] && exiftool -AllDates+="$OFFSET" -IFD1:ModifyDate+="$OFFSET" -FileModifyDate+="$OFFSET" -overwrite_original_in_place -r $*
 exiftool '-FileName<CreateDate' -d "IMG_%Y%m%d_%H%M%S%%-c${SUFFIX}.%%e" -overwrite_original_in_place $RECURSIVE $*
 if [ $RESIZE ]; then
     IFS=`echo`
