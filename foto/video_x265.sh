072a74bac502b3a796c72fcc6a432b90b0cb0f32
Lukáš Doktor <ldoktor@redhat.com>
2017-03-28 09:14:58 +0200

    foto.video_x265: Convert videos to x265
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

---
+++ foto/video_x265.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+while [ true ]; do
+	case $1 in
+		"-r")
+			RECURSIVE="-r"
+			shift
+			;;
+        "--crf"|"-c")
+            shift
+            CRF=$1
+            shift
+            ;;
+        "--delete"|"-d")
+            DELETE="yes"
+            shift
+            ;;
+		"-h")
+			echo "Usage: $0 [-h] [-r] [-c] [-o] file [file ...]"
+			echo
+			echo "  file   File(s) to be proceed"
+			echo "  -h     Show this help"
+            echo "  -r     Recursive"
+            echo "  -c     conversion video quality (crf)"
+            echo "  -d     delete original file"
+			exit 1
+			;;
+		*)
+			break
+		shift
+	esac
+done
+
+[ ! "$CRF" ] && CRF=23
+
+convert() {
+    if file "$FILE" | grep "ISO Media, " &>/dev/null; then
+        if [[ "$FILE" == *x265.mp4 ]]; then
+            echo "SKIP already x265: $1"
+            return
+        fi
+        if [ -e "$FILE.x265.mp4" ]; then
+            echo "SKIP x265 exists: $1"
+            return
+        fi
+        # Do the conversion
+        echo "Processing $1"
+        ffmpeg -nostdin -i "$1" -c:v libx265 -preset veryslow -crf $CRF -c:a aac -b:a 128k "$FILE.x265.mp4" &> convert.log
+        if [ ${PIPESTATUS[0]} -ne 0 ]; then
+            cat convert.log
+            exit -10
+        else
+            rm convert.log
+            [ "$DELETE" == "yes" ] && rm "$1"
+        fi
+    else
+        #echo "SKIP not video: $1"
+        return
+    fi
+}
+
+if [ "$RECURSIVE" ]; then
+    find $* -type f -print0 | while read -d $'\0' FILE; do
+        convert "$FILE"
+    done
+else
+    for FILE in $*; do
+        convert "$FILE"
+    done
+fi
