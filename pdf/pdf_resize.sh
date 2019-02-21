170fbe3201c296eb74bd853920590c3862573f6b
Lukáš Doktor <ldoktor@redhat.com>
2019-02-21 16:26:09 +0100

    pdf: Add some useful commands
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- pdf/pdf_resize.sh
+++ pdf/pdf_resize.sh
@@ -1,1 +1,12 @@
+# DEFAULT RESIZE
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf input.pdf
+# Black&white + ebook size
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sColorConversionStrategy=Gray -dProcessColorModel=/DeviceGray -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output2.pdf naskenováno_20190221-1415.pdf
+# Just resize to ebook size
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -
+# Sizes are:
+#    -dPDFSETTINGS=/screen lower quality, smaller size. (72 dpi)
+#    -dPDFSETTINGS=/ebook for better quality, but slightly larger pdfs. (150 dpi)
+#    -dPDFSETTINGS=/prepress output similar to Acrobat Distiller "Prepress Optimized" setting (300 dpi)
+#    -dPDFSETTINGS=/printer selects output similar to the Acrobat Distiller "Print Optimized" setting (300 dpi)
+#    -dPDFSETTINGS=/default selects output intended to be useful across a wide variety of uses, possibly at the expense of a larger output file
---
+++ pdf/pdf_sign.sh
@@ -0,0 +1,4 @@
+# sign:
+gpg --clearsign --output=signed.pdf input.pdf
+# verify
+$ gpg --verify signed.pdf
