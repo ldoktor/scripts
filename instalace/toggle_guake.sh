a505fe6eff7de9a010c01d18f1a82449391623c2
Lukáš Doktor <ldoktor@redhat.com>
2020-08-28 15:08:48 +0200

    instalace: Modify toggle_guake to python3
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

--- instalace/toggle_guake.sh
+++ instalace/toggle_guake.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8; -*-
 # Copied from https://github.com/Guake/guake/issues/492#issuecomment-324165795
 import dbus
 
 try:
     bus = dbus.SessionBus()
-    remote_object = bus.get_object('org.guake.RemoteControl', '/org/guake/RemoteControl')
+    remote_object = bus.get_object('org.guake3.RemoteControl', '/org/guake3/RemoteControl')
     remote_object.show_hide()
-except dbus.DBusException:
-    pass
+except dbus.DBusException as details:
+    print(details)
