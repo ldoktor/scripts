9282e786464ad80d87373ae3d31e6c53a79e4439
Lukáš Doktor <ldoktor@redhat.com>
2026-05-11 10:35:32 +0200

    Gnome suspend helper
    ensure to log the display
    
    Signed-off-by: Lukáš Doktor <ldoktor@redhat.com>

---
+++ gnome-suspend.sh
@@ -0,0 +1,1 @@
+gnome-screensaver-command --lock && dbus-send --system --print-reply   --dest="org.freedesktop.login1"   /org/freedesktop/login1   org.freedesktop.login1.Manager.Suspend boolean:true
